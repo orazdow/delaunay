3426c37be27f2303bcb4d0776b7239966112a158
ollie razdow <ollie razdow>
2017-04-07 00:22:13 -0400

    animation parameters

--- sketch.js
+++ sketch.js
@@ -5,13 +5,13 @@ var labels = 0;
 
 var ww = 800;
 var wh = 700;
-var view = 0;
+var view = 1;
 var tsize = 3000;
-var t, s1, s2, s3;
+var st, s1, s2, s3;
 var a = 0;
 var b = 0;
 function setup(){
-
+noiseDetail(5, 0.3);
 //frameRate(30);
 //noLoop();
 createCanvas(ww, wh);
@@ -27,14 +27,15 @@ function draw(){
 	background(100);
     triangles.triangles = [];
     triangles.edgetriangles = [];
- st = addVertices(-tsize, wh+tsize, ww/2, -tsize, ww+tsize, wh+tsize);
-st.boundary = true;
-s1 = st.a; s2 = st.b; s3 = st.c;   
+    st = addVertices(-tsize, wh+tsize, ww/2, -tsize, ww+tsize, wh+tsize);
+    st.boundary = true;
+    s1 = st.a; s2 = st.b; s3 = st.c;   
 
-a += 0.003;
-for (var i = 0; i < 20; i++){
-
-	add_split( noise(a, i)*ww, noise(i, a)*ww)
+	a += 0.002;
+	for (var i = 0; i < 30; i++){
+	b+=0.00001;
+	 add_split(noise(i+b, a)*ww*1.3,noise(2000+i+b, 2000+a+b)*wh*1.3);
+	//add_split( noise(a, i)*ww, noise(i, a)*ww)
    }
 
 display();
@@ -50,6 +51,7 @@ for(key in triangles.triangles){
 	if(!t.boundary){
 		if(view === 0 || view === 2){
 		stroke(255);
+		 fill(t.center.r)
          triangle(t.a.x, t.a.y, t.b.x, t.b.y, t.c.x, t.c.y); 
          }
          if(view === 1 || view === 2){
@@ -77,3 +79,9 @@ display();
 
 }
 
+// a += 0.003;
+
+// for (var i = 0; i < 80; i++) {
+// 	b+=0.00001;
+// 	ellipse(noise(i+b, a)*w*1.3,noise(2000+i+b, 2000+a+b)*w*1.3,5, 5);
+// }
