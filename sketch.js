4c9259dea5d39a1af098ff330f087b05895e843d
ollie razdow <ollie razdow>
2017-04-07 20:47:46 -0400

    ok

--- sketch.js
+++ sketch.js
@@ -1,34 +1,25 @@
 var triangles = new TriangleTable();
 var labels = 0;
-
 var ww = 800;
 var wh = 700;
 var view = 1;
-
 var st, s1, s2, s3;
 var a = 0;
 var b = 0;
 
 function setup(){
-//noLoop();
+pixelDensity(1);
 noiseDetail(5, 0.3);
 createCanvas(ww, wh);
-background(100);
-stroke(255);
-st = addVertices(-3000, wh+3000, ww/2, -3000, ww+3000, wh+3000);
-st.boundary = true;
-s1 = st.a; s2 = st.b; s3 = st.c;
 }
 
 function draw(){ 
 background(100);
 reset();
-
-a += 0.002;
+a += 0.002; //b+=0.0002;
 for (var i = 0; i < 30; i++){
 	b+=0.00001;
  	add_split(noise(i+b, a)*ww*1.3,noise(2000+i+b, 2000+a+b)*wh*1.3);
-	//add_split( noise(a, i)*ww, noise(i, a)*ww)
 }
 display();
 }
@@ -36,25 +27,24 @@ display();
 function display(){ 
 background(100);
 noFill();
-
 for(key in triangles.triangles){
-	var t = triangles.triangles[key]
-	 setNeighbors(t)
-	if(!t.boundary){
-		if(view === 0 || view === 2){
-		stroke(255);
-		 fill(t.center.r)
-         triangle(t.a.x, t.a.y, t.b.x, t.b.y, t.c.x, t.c.y); 
-         }
-         if(view === 1 || view === 2){
-		stroke(255, 0, 0);
-		 ellipse(t.a.x, t.a.y, 6,6); ellipse(t.b.x, t.b.y, 6,6); ellipse(t.c.x, t.c.y, 6,6);
-		// ellipse(t.center.x, t.center.y, t.center.r*2, t.center.r*2)
-		 line(t.center.x, t.center.y, t.vA.x, t.vA.y);
- 		 line(t.center.x, t.center.y, t.vB.x, t.vB.y);
-		 line(t.center.x, t.center.y, t.vC.x, t.vC.y);
-		 }
-	}
+var t = triangles.triangles[key]
+ setNeighbors(t)
+if(!t.boundary){
+	if(view === 0 || view === 2){
+	stroke(255);
+	 fill(t.center.r)
+     triangle(t.a.x, t.a.y, t.b.x, t.b.y, t.c.x, t.c.y); 
+     }
+     if(view === 1 || view === 2){
+	stroke(255, 0, 0);
+	 ellipse(t.a.x, t.a.y, 6,6); ellipse(t.b.x, t.b.y, 6,6); ellipse(t.c.x, t.c.y, 6,6);
+	// ellipse(t.center.x, t.center.y, t.center.r*2, t.center.r*2)
+	 line(t.center.x, t.center.y, t.vA.x, t.vA.y);
+		 line(t.center.x, t.center.y, t.vB.x, t.vB.y);
+	 line(t.center.x, t.center.y, t.vC.x, t.vC.y);
+	 }
+}
 }
 
 }
@@ -62,7 +52,7 @@ for(key in triangles.triangles){
 function keyPressed() {
   if (key === ' ') {
   view = ++view%3;
-} display()
+} //display();
 }
 
 function mousePressed(){
